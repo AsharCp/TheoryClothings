87111e9f48418b6ee98cfeefb6a318e27bee61aa
Ashar Cp <asharjamancpkvr@gmail.com>
2025-05-24 12:23:52 +0530

    Small chane in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex flex-row sm:justify-between items-center md:px-8 border-b-2'>
-        <div className='bg-white font-mono sw-full h-fit p-4 md:p-6 bg-whites font-bold text-2xl sm:text-3xl md:text-4xl'>THEORY.clothings</div>
-        <button className='bg-black invisible sm:visible text-white md:py-2 px-2 text-center md:px-4 text-lg rounded hover:bg-gray-200 hover:text-black'>Contact</button>
-        
-        
-    </div>
+    <div className='flex items-center justify-center sm:justify-between border-b-2 px-4 md:px-8 py-4'>
+      <div className='font-mono font-bold text-3xl sm:text-3xl md:text-4xl text-center w-full sm:w-auto'>THEORY.clothings</div>
+      <button className='hidden sm:block bg-black text-white md:py-2 px-2 md:px-4 text-lg rounded hover:bg-gray-200 hover:text-black'>Contact</button>
+ </div>
     
   )
 }
